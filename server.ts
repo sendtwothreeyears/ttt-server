979fec978bfae67d258e1fe090726a32e60d1890
work <work@fss-MacBook-Pro.local>
2026-02-06 18:24:59 -0500

    [edit] edited server for deployment

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import tttRouter from "./controllers/tttRouter";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 const router = express.Router();
 
