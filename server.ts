7f22e215cb3ebd6aaa0fe34825662be4a1e4b5c5
work <work@fss-MacBook-Pro.local>
2026-02-05 19:40:16 -0500

    [edit] added api endpoint test coverage

--- server.ts
+++ server.ts
@@ -1,17 +1,6 @@
-import express from "express";
-import cors from "cors";
-import tttRouter from "./controllers/tttRouter";
+import app from "./app";
 
-const app = express();
 const port = 3001;
-
-const router = express.Router();
-
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-
-router.use("/api", tttRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
